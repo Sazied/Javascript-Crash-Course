52cb52275ef4b39f7f4bdf02392efeda4f7c960e
Sazied Hassan <mshassan@mun.ca>
2020-08-28 12:25:51 -0230

    function
    dataTypes

--- home.js
+++ home.js
@@ -50,3 +50,69 @@ as shown!
 }
 
 fun();
+
+/* Create a function that displays 
+your name in the console log  after you 
+put it in*/
+
+const greeting = (yourName) => {
+    let result = 'Hello ' + yourName;
+    console.log (result);
+    document.getElementById('someText').textContent = result;
+}
+
+// let name = prompt('What is your name?');
+
+// greeting(name);
+
+// How do arguments work in fuctions? 
+// How do we add 2 numbers together in a function? 
+
+const addNumber = (num1, num2) => {
+    let result = num1 + num2;
+    console.log(result); 
+}
+
+addNumber(4,3);
+
+// While loops
+
+// let num = 0;
+
+// while (num < 100) {
+//     num += 3;
+//     console.log(num);
+// }
+
+// For loop 
+
+for (let num = 0; num <= 100; num+=20) {
+    console.log(num);
+}
+
+// Data types
+let yourAge = 18; //number 
+let yourName = 'Sazied'; //string 
+let name = {first: 'Sazied', last: 'Hassan'}; // object 
+let truth = false; // boolean 
+let groceries = ['apple', 'banana', 'orange']; // array
+let random; // undefined
+let nothing = null; // value null
+
+// String in Javascript (common methods)
+let fruit = 'banana, apple, orange, blackberry';
+let moreFruits = 'banana\napple'; // new line 
+console.log(fruit, moreFruits);
+
+console.log(moreFruits.length);
+console.log(fruit.indexOf('n')); // (starts from 0)
+console.log(fruit.slice(2, 4)); //(including, up to)
+console.log(fruit.replace('ban', '123')); 
+console.log(fruit.toUpperCase());
+console.log(fruit.toLowerCase()); 
+console.log(fruit.charAt(2));
+console.log(fruit[2]);
+console.log(fruit.split(''));
+console.log(fruit.split(',')); //split by a comma
+
+// Array
